cab553706481f5647df0ab375030d649079a971f
Mohamed-Khamis2294 <Mohamed.Khamis2294@gmail.com>
2024-08-27 21:54:21 +0300

    fix somethings

--- src/componants/Verify.jsx
+++ src/componants/Verify.jsx
@@ -61,7 +61,9 @@ export default function Verify() {
     
     
     <button type="submit" className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-blue-800">
-    {clicked?<Loading color={'#fff'} size={'24px'}/>:'verify'} 
+    {clicked?<div className='flex justify-center items-center w-full'>
+    <Loading  size={'30px'}/>
+  </div>:'verify'} 
     </button>
    
   </form>
